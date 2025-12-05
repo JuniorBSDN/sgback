ab79e9487704237d35179290c590d1bb593bfebc
Airton JR <123423577+JuniorBSDN@users.noreply.github.com>
2025-12-05 17:38:50 -0300

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -4,9 +4,3 @@ Servidores web (como Gunicorn, Apache com mod_wsgi, Vercel)
 irão procurar pela variável 'application'.
 """
 from app import app as application
-
-
-if __name__ == "__main__":
-    # Permite rodar diretamente com `python wsgi.py` em desenvolvimento,
-    # embora o comando `flask run` ou `python app.py` seja mais comum.
-    application.run()
